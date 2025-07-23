3c4f8396fb1820db6a36904befaa124a4161ac79
rohitmi0023 <rohitracer0023@gmail.com>
2025-07-23 10:23:13 +0530

    taxi_etl_project- modelling start and pandas setup

---
+++ main.py
@@ -0,0 +1,21 @@
+# %%
+# !pip3 install pandas
+
+# %%
+import pandas as pd
+
+df = pd.read_csv('taxi_data.csv')
+
+# development
+pd.set_option('display.max_columns', None) # show all columns
+pd.set_option('display.max_rows', 100) # show 100 rows
+pd.set_option('display.width', None) # auto width
+
+# creating a unique identifier for each row
+df['trip_id'] = df.index + 1
+
+# convert tpep_pickup_datetime, tpep_dropoff_datetime from object to datetime in a single line
+df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']] = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']].apply(pd.to_datetime)
+
+df['tpep_pickup_datetime'].dt.day.drop_duplicates()
+# get unique pickup days
